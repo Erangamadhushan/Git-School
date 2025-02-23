64d38e2644eabe7b62d38c5ca61949b0da0cd6a1
Erangamadhushan <erangamadhushann956@gmail.com>
2025-02-23 11:54:41 +0530

    set responsiveness into the main direction property

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,7 @@ function renderMainDirection(contents) {
                 <div class="card-body">
                     <h2 class="card-title text-white">${content.category}</h2>
                     <p class="card-text py-5 text-white">${content.title}</p>
-                    <a href="" class="btn btn-outline-alice mb-3">Read More</a>
+                    <a href="#" class="btn btn-outline-neon mb-3 my-4">Read More</a>
                 </div>
             </div>
         `;
@@ -24,7 +24,7 @@ function renderMainDirection(contents) {
 async function renderLoadConfirmation() {
     const { value: accept } = await Swal.fire({
         title: "Terms and conditions",
-        backgroundColor:'#222',
+        // backgroundColor:'#222',
         input: "checkbox",
         inputValue: 1,
         inputPlaceholder: `
