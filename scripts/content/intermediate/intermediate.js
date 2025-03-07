6354fc30200e36e93cde64d6da73f7d3feb0cc75
Erangamadhushan <erangamadhushann956@gmail.com>
2025-03-07 17:55:22 +0530

    Add responsive navigation menu and enhance navbar content across all guides

--- scripts/content/intermediate/intermediate.js
+++ scripts/content/intermediate/intermediate.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    renderNavbarContent();
     const root = document.querySelector('#root');
     let mainContent = `<h1 class=" py-5 text-[2em] md:text-[3em] text-center text-white">A Complete Guide to Git and GitHub</h1>`;
 
@@ -120,4 +121,20 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    function renderNavbarContent() {
+        const menuList = document.querySelector('.menu-list');
+        let menuContent = `
+            <ul class="grid w-[100%]">
+                <li class="w-[90%] mx-auto group hover:bg-gray-900 hover:text-white text-center my-1 py-3"><a href="./beginner.html" class="p-1 text-center w-[100%] text-gray-900 bg-white group-hover:bg-gray-900 group-hover:text-white">Beginner Level</a></li>
+                <li class="w-[90%] mx-auto group hover:bg-gray-900 hover:text-white text-center my-3 py-3"><a href="./intermediate.html" class="p-1 text-center w-[100%] text-gray-900 bg-white group-hover:bg-gray-900 group-hover:text-white">Intermediate Level</a></li>
+                <li class="w-[90%] mx-auto group hover:bg-gray-900 hover:text-white text-center my-3 py-3"><a href="./expert.html" class="p-1 text-center w-[100%] text-gray-900 bg-white group-hover:bg-gray-900 group-hover:text-white">Expert Level</a></li>
+            </ul>
+            <div class="flex">
+                <p class="text-gray-900 ml-3 text-lg fond-bold">Contribute To :</p>
+                <a href="https://github.com/Erangamadhushan/Git-School" target="_blank"><img src="./assets/icons/github.png"/></a>
+            </div>
+        `;
+
+        menuList.innerHTML = menuContent;
+    }
 });
